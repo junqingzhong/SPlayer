7943b56dbbe60339bfc16ac36856a6895d7d6fe6
imsyy <one@imsyy.top>
2025-10-27 00:46:02 +0800

    🐞 fix: 修复工具栏图标 & 添加调试工具

--- src/views/DesktopLyrics/index.vue
+++ src/views/DesktopLyrics/index.vue
@@ -1,3 +1,43 @@
 <template>
-  <div class="desktop-lyrics"></div>
+  <div ref="desktopLyricsRef" class="desktop-lyrics">
+    {{ lyricData.playName }}
+    {{ lyricConfig }}
+  </div>
 </template>
+
+<script setup lang="ts">
+import type { LyricType } from "@/types/main";
+
+// 桌面歌词数据
+const lyricData = reactive<{
+  playName: string;
+  playStatus: boolean;
+  progress: number;
+  lrcData: LyricType[];
+  yrcData: LyricType[];
+  lyricIndex: number;
+}>({
+  playName: "未知歌曲",
+  playStatus: false,
+  progress: 0,
+  lrcData: [],
+  yrcData: [],
+  lyricIndex: -1,
+});
+
+// 桌面歌词配置
+const lyricConfig = reactive<{
+  fontSize: number;
+  lineHeight: number;
+}>({
+  fontSize: 24,
+  lineHeight: 48,
+});
+</script>
+
+<style scoped lang="scss">
+.desktop-lyrics {
+  color: #fff;
+  background-color: transparent;
+}
+</style>
