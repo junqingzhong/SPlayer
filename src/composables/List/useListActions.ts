d02805b24de4b67014116532ec5e6597db7f1842
kazukokawagawa <2580099704@qq.com>
2025-12-20 05:39:49 +0800

    🎈 perf: 优化歌单对于单曲循环的自动切换

--- src/composables/List/useListActions.ts
+++ src/composables/List/useListActions.ts
@@ -1,17 +1,23 @@
 import type { SongType } from "@/types/main";
 import { usePlayerController } from "@/core/player/PlayerController";
+import { useStatusStore } from "@/stores";
 
 /**
  * 列表操作逻辑
  */
 export const useListActions = () => {
   const player = usePlayerController();
+  const statusStore = useStatusStore();
 
   /**
    * 播放全部歌曲
    */
   const playAllSongs = (songs: SongType[], playListId?: number) => {
     if (!songs?.length) return;
+    // 如果是单曲循环模式，自动切换为顺序播放
+    if (statusStore.playSongMode === "repeat-once") {
+      player.togglePlayMode("repeat");
+    }
     player.updatePlayList(songs, undefined, playListId);
   };
 
