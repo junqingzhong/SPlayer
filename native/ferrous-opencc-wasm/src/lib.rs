705175321379920bf67b43a91acf7e1a4166b16a
apoint123 <108002475+apoint123@users.noreply.github.com>
2026-01-17 00:39:28 +0800

    📄docs: 更多文档

--- native/ferrous-opencc-wasm/src/lib.rs
+++ native/ferrous-opencc-wasm/src/lib.rs
@@ -4,6 +4,8 @@ use ferrous_opencc::{
 };
 use wasm_bindgen::prelude::*;
 
+/// 一个包装了 [`OpenCC`] 转换器的结构体
+/// @see {@link <https://docs.rs/ferrous-opencc/0.3.1/ferrous_opencc/struct.OpenCC.html>}
 #[wasm_bindgen]
 pub struct TextConverter {
     inner: OpenCC,
@@ -11,8 +13,12 @@ pub struct TextConverter {
 
 #[wasm_bindgen]
 impl TextConverter {
+    /// 使用指定的配置文件创建一个 `TextConverter` 实例
+    /// @param `config_name` 内置配置文件的名称，参见 {@link <https://docs.rs/ferrous-opencc/0.3.1/ferrous_opencc/config/enum.BuiltinConfig.html>}
+    /// @returns 返回初始化的转换器实例
+    /// @throws 如果传入的配置名称不存在或加载失败，抛出错误
     #[wasm_bindgen(constructor)]
-    #[allow(clippy::missing_errors_doc)]
+    #[allow(clippy::missing_errors_doc)] // 有了
     pub fn new(config_name: &str) -> Result<Self, JsValue> {
         let config_enum = BuiltinConfig::from_filename(config_name)
             .map_err(|e| JsValue::from_str(&e.to_string()))?;
@@ -21,6 +27,8 @@ impl TextConverter {
         Ok(Self { inner })
     }
 
+    /// 根据之前初始化时的配置进行简繁转换
+    /// @see OpenCC.convert
     #[wasm_bindgen]
     #[must_use]
     pub fn convert(&self, input: &str) -> String {
