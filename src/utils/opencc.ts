18353559cd2ca4049f8fb8112e274f6750913653
apoint123 <108002475+apoint123@users.noreply.github.com>
2026-01-17 00:27:01 +0800

    🦄 refactor: 优化初始化 wasm 的竞态条件

--- src/utils/opencc.ts
+++ src/utils/opencc.ts
@@ -4,6 +4,25 @@ type WasmModule = typeof import("@opencc");
 let wasmModule: WasmModule | null = null;
 // 缓存所有已创建的转换器实例，考虑到转换模式只有十几种，全部换成下来的开销应该不大
 const converters = new Map<string, TextConverter>();
+let wasmInitialized: Promise<void> | null = null;
+
+async function initializeWasm() {
+  if (wasmModule) return;
+
+  if (!wasmInitialized) {
+    wasmInitialized = (async () => {
+      try {
+        wasmModule = await import("@opencc");
+        await wasmModule.default();
+      } catch (e) {
+        wasmInitialized = null;
+        wasmModule = null;
+        throw e;
+      }
+    })();
+  }
+  await wasmInitialized;
+}
 
 // CAUTION: 更改了 wasm 的编译参数后务必更新它
 export type ConverterMode =
@@ -25,6 +44,7 @@ export type ConverterMode =
  * 为指定的简繁转换模式创建一个转换器
  * @param mode 转换模式 (例如 "s2t", "t2s")。参见 {@linkcode ConverterMode}
  * @returns 一个接受字符串并返回转换后字符串的函数
+ * @throws 初始化 wasm 失败时抛出错误
  */
 export async function getConverter(mode: ConverterMode): Promise<(text: string) => string> {
   if (mode === "off") {
@@ -32,9 +52,10 @@ export async function getConverter(mode: ConverterMode): Promise<(text: string) 
   }
 
   try {
+    await initializeWasm();
+
     if (!wasmModule) {
-      wasmModule = await import("@opencc");
-      await wasmModule.default();
+      throw new Error("OpenCC Wasm 加载失败");
     }
 
     let converterInstance = converters.get(mode);
